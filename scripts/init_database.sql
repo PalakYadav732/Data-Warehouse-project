e5c6495fa3a471cb4c3d397e39ca476c00cc3a24
PalakYadav732 <82311008+PalakYadav732@users.noreply.github.com>
2026-01-03 02:12:40 +0530

    Create init_database.sql for DataWarehouse setup
    This script creates a new database named 'DataWarehouse' and sets up three schemas: 'bronze', 'silver', and 'gold'. It also includes a warning about data loss if the database already exists.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,18 @@
+/*
+=======================================================================
+Create Database and Schemas 
+=======================================================================
+Script Purpose:
+      This script creates a new database named 'DataWarehouse' after checking if it already exists.
+      If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+      within the database: 'bronze','silver','gold'.
+
+WARNING:
+      Running this script will drop the entire 'DateWarehouse' database if it exists.
+      All data in the database will permanently deleted. Proceed with caution
+      and ensure you have backups before running this script.
+*/
+
 USE master;
 
 CREATE DATABASE DataWarehouse;
